126043f50541e79433a9ac5831b3ced4cc9b87af
Traasko <56584321+Traasko@users.noreply.github.com>
2020-02-23 00:05:34 +0100

    2.4

--- restauration/inscription.php
+++ restauration/inscription.php
@@ -7,28 +7,28 @@ class inscription{
   private $mail;
   private $mdp;
 }
-public function __construct($nom, $prenom, $mail, $mdp){
+  public function __construct($nom, $prenom, $mail, $mdp){
   $this->setNom($nom);
   $this->setPrenom($prenom);
   $this->setMail($mail);
   $this->setMdp($mdp);
 }
 
-public function getNom() { return $this->nom;}
-public function getPrenom() { return $this->prenom;}
-public function getMail() {  return $this->mail;}
-public function getMdp() {  return $this->mdp;}
+  public function getNom() { return $this->nom;}
+  public function getPrenom() { return $this->prenom;}
+  public function getMail() {  return $this->mail;}
+  public function getMdp() {  return $this->mdp;}
 
-public function setNom($nom){
+  public function setNom($nom){
   $this->nom = $nom;
 }
-public function setPrenom($prenom){
+  public function setPrenom($prenom){
   $this->prenom = $prenom;
 }
-public function setMail($mail){
+  public function setMail($mail){
   $this->mail = $mail;
 }
-public function setMdp($mdp){
+  public function setMdp($mdp){
   $this->mdp = $mdp;
 }
 
