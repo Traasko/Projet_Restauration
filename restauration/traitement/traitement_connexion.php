600d5c28d40b002d0f48d3ace8ecceabbf947f5c
Traasko <56584321+Traasko@users.noreply.github.com>
2020-02-25 19:46:37 +0100

    2.7

--- restauration/traitement/traitement_connexion.php
+++ restauration/traitement/traitement_connexion.php
@@ -3,7 +3,7 @@
 require('../manager/manager_connexion.php');
 require('../model/class_connexion.php');
 
-$user = new Connexion($_POST['mail'], $_POST['mdp']);
+$user = new Connexion($_POST['nom'], $_POST['mdp']);
 $manager = new Manager();
 $manager->connexion($user);
 
